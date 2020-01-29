274acae3a41f7fcc77d46e7ee5782ed4580c948d
simon <simon.thevenin@imt-atlantique.fr>
2020-01-29 12:34:10 +0100

    modify shell for unix

--- main.cpp
+++ main.cpp
@@ -34,23 +34,26 @@ Data * getData(string file, int NbPeriod, int NbSupplier)
 
 
 
-void runExact(string file, int NbPeriod, int NbSupplier, double gamma)
+void runExact(string file, int NbPeriod, int NbSupplier, double gamma, bool warmstart)
 {
     Data *data = getData(file, NbPeriod, NbSupplier);
     ModelQuantity* ModQ= new ModelQuantity(data, gamma);
     ModQ->BuildModel();
+    string name= "ExactNoWarmStart";
+    if(warmstart) {
+           name= "Exact";
+          int** givenY2 = new int*[data->getNSup()];
+          for(int s=0; s<data->getNSup(); s++)
+          {
+              givenY2[s] =  new int[data->getNPer()];
+              for(int t=0; t<data->getNPer(); t++)
+              {
+                  givenY2[s][t] =1;
+              }
+          }
 
-  /*  int** givenY2 = new int*[data->getNSup()];
-    for(int s=0; s<data->getNSup(); s++)
-    {
-        givenY2[s] =  new int[data->getNPer()];
-        for(int t=0; t<data->getNPer(); t++)
-        {
-            givenY2[s][t] =1;
-        }
+          ModQ->Solve(true, givenY2, false, 0.01);
     }
-
-    ModQ->Solve(true, givenY2, false, 0.01);*/
     double cost = ModQ->Solve(false, nullptr, false, 0.0001);
     int** obtainedY2 = new int*[data->getNSup()+1];
     for(int s=0; s<data->getNSup(); s++)
@@ -62,7 +65,7 @@ void runExact(string file, int NbPeriod, int NbSupplier, double gamma)
         }
     }
     string FFile = pathfile + "resultat7.txt";
-    data->Affich_Results(FFile,   gamma, "ExactNoWarmStart", obtainedY2, cost, ModQ->LastRunning, ModQ->LastGap, -1, -1,ModQ->GetInventoryCosts(), ModQ->GetAvgInventory(), ModQ->GetPurshasingCosts(), ModQ->GetBackorderCosts(), ModQ->GetAvgtBackorder());
+    data->Affich_Results(FFile,   gamma, name, obtainedY2, cost, ModQ->LastRunning, ModQ->LastGap, -1, -1,ModQ->GetInventoryCosts(), ModQ->GetAvgInventory(), ModQ->GetPurshasingCosts(), ModQ->GetBackorderCosts(), ModQ->GetAvgtBackorder());
     cout<<"optimal cost::::"<<cost<<endl;
     cout<<"Inv Cost:"<<ModQ->GetInventoryCosts()<<" Avg Inv:"<<ModQ->GetAvgInventory()<<" Order Cost:"<<ModQ->GetOrderingCosts()<<endl;
     cout<<"Back Cost:"<<ModQ->GetBackorderCosts()<<" Avg Back:"<<ModQ->GetAvgtBackorder()<<" Pursh cost:"<<ModQ->GetPurshasingCosts()<<endl;
@@ -174,10 +177,11 @@ void runDeterministic(string file, int NbPeriod, int NbSupplier, int gamma)
 }
 
 int mainSimon(string file, int nbp, int nbs, int gamma) {
-   // runRobust(file, nbp, nbs,gamma);
-    runExact(file, nbp, nbs,gamma);
-   // runGrasp(file, nbp, nbs,gamma);
-   // runDeterministic(file, nbp, nbs,gamma);
+    runRobust(file, nbp, nbs,gamma);
+    runExact(file, nbp, nbs,gamma, true);
+    runExact(file, nbp, nbs,gamma, false);
+    runGrasp(file, nbp, nbs,gamma);
+    runDeterministic(file, nbp, nbs,gamma);
 
 }
 
